cf76080f3921e0e14fe83df150c052fbeb3bb492
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-06 21:38:15 +0000

    refactors out rendering of templates

--- src/main/kotlin/super_springboard_snowdown/controllers/SnowboarderController.kt
+++ src/main/kotlin/super_springboard_snowdown/controllers/SnowboarderController.kt
@@ -1,13 +1,11 @@
 package super_springboard_snowdown.controllers
 
-import com.mitchellbosecke.pebble.PebbleEngine
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import super_springboard_snowdown.Snowboarder
 import super_springboard_snowdown.SnowboarderService
-import java.io.StringWriter
 
 @RestController
 class SnowboarderController(val service: SnowboarderService) {
@@ -26,12 +24,4 @@ class SnowboarderController(val service: SnowboarderService) {
     fun post(@RequestBody snowboarder: Snowboarder) {
         service.post(snowboarder)
     }
-
-    private fun renderTemplate(templateName:String, templateArguments:Map<String, List<Any>>?=null):String {
-        val template = PebbleEngine.Builder().build().getTemplate("templates/$templateName.peb")
-        val writer = StringWriter()
-        template.evaluate(writer, templateArguments)
-        return writer.toString()
-    }
-
 }
