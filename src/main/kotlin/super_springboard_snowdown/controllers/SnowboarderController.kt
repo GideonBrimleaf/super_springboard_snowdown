e03b6b93b3d8dafd72eb326ec30441b3e4eda903
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-07 22:31:05 +0000

    Can create new snowboarders with form

--- src/main/kotlin/super_springboard_snowdown/controllers/SnowboarderController.kt
+++ src/main/kotlin/super_springboard_snowdown/controllers/SnowboarderController.kt
@@ -1,6 +1,9 @@
 package super_springboard_snowdown.controllers
 
+import org.springframework.ui.ModelMap
+import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.RedirectView
 import super_springboard_snowdown.models.Snowboarder
 import super_springboard_snowdown.repositories.SnowboarderService
 
@@ -19,6 +22,17 @@ class SnowboarderController(val service: SnowboarderService) {
     }
 
     @PostMapping("/snowboarders")
+    fun create(@RequestBody formData:MultiValueMap<String, String>):RedirectView {
+        val name = formData["name"]?.first()!!
+        val age = formData["age"]?.first()?.toInt()!!
+        val profile = formData["profile"]?.first()!!
+
+        val newSnowboarder = Snowboarder(name = name, age = age, profile = profile)
+        service.post(newSnowboarder)
+        return RedirectView("/snowboarders")
+    }
+
+    @PostMapping("/api/snowboarders")
     fun post(@RequestBody snowboarder: Snowboarder) {
         service.post(snowboarder)
     }
--- src/main/kotlin/super_springboard_snowdown/models/Snowboarder.kt
+++ src/main/kotlin/super_springboard_snowdown/models/Snowboarder.kt
@@ -4,4 +4,4 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("SNOWBOARDERS")
-data class Snowboarder(@Id val id: String?, val name: String, val age:Int, val profile:String)
+data class Snowboarder(@Id val id:String?=null, val name: String, val age: Int, val profile: String)
