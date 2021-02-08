d6ffae56518543de481f3cc04765fcf29333b386
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-08 21:14:50 +0000

    adds event model - needs tested

---
+++ src/main/kotlin/super_springboard_snowdown/models/Event.kt
@@ -0,0 +1,7 @@
+package super_springboard_snowdown.models
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("EVENTS")
+data class Event(@Id val id:String?, val name:String, val location:String, val prizeMoney:Int)
