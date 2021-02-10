ec8186f242ca8f2a1e2daca70d3336c38b461ea0
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-10 17:37:18 +0000

    adds some commentary explaining why this is necessary

--- src/main/kotlin/super_springboard_snowdown/repositories/EventRepository.kt
+++ src/main/kotlin/super_springboard_snowdown/repositories/EventRepository.kt
@@ -3,4 +3,6 @@ package super_springboard_snowdown.repositories
 import org.springframework.data.repository.CrudRepository
 import super_springboard_snowdown.models.Event
 
+// We can't straight up use the CrudRepository interface directly
+// So we need this even if it's empty
 interface EventRepository: CrudRepository<Event, String>
