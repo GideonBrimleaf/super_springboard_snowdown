eaff043db6a6fcbc4cb896e79d437653fc44b685
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-11 21:19:06 +0000

    tweaks jpa repo comments

--- src/main/kotlin/super_springboard_snowdown/repositories/EventRepository.kt
+++ src/main/kotlin/super_springboard_snowdown/repositories/EventRepository.kt
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import super_springboard_snowdown.models.Event
 
-// We can't straight up use the CrudRepository interface directly
+// We can't straight up use the JPA Repository interface directly
 // So we need this even if it's empty
 interface EventRepository: JpaRepository<Event, String>
