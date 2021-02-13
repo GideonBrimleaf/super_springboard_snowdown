905dd5ca754322990eebd672bfa59752f92afe08
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-13 21:19:50 +0000

    reformats models for readibility

--- src/main/kotlin/super_springboard_snowdown/models/Event.kt
+++ src/main/kotlin/super_springboard_snowdown/models/Event.kt
@@ -2,18 +2,11 @@ package super_springboard_snowdown.models
 
 import javax.persistence.*
 
-@Entity
-@Table(name="events")
+@Entity @Table (name="events")
 data class Event (
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id:Long?=null,
-    @Column(name="name")
-    val name:String,
-    @Column(name="location")
-    val location:String,
-    @Column(name="prize_money")
-    val prizeMoney:Int,
-    @ManyToMany(mappedBy = "events")
-    val snowboarders:List<Snowboarder> = mutableListOf()
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY) val id:Long?=null,
+    @Column (name="name") val name:String,
+    @Column (name="location") val location:String,
+    @Column (name="prize_money") val prizeMoney:Int,
+    @ManyToMany (mappedBy = "events") val snowboarders:List<Snowboarder> = mutableListOf(),
 )
--- src/main/kotlin/super_springboard_snowdown/models/Snowboarder.kt
+++ src/main/kotlin/super_springboard_snowdown/models/Snowboarder.kt
@@ -2,23 +2,15 @@ package super_springboard_snowdown.models
 
 import javax.persistence.*
 
-@Entity
-@Table(name="snowboarders")
+@Entity @Table (name="snowboarders")
 data class Snowboarder (
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id:Long?=null,
-    @Column(name="name")
-    val name: String,
-    @Column(name="age")
-    val age: Int,
-    @Column(name="profile")
-    val profile: String,
-    @ManyToMany
-    @JoinTable(
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY) val id:Long?=null,
+    @Column (name="name") val name: String,
+    @Column (name="age") val age: Int,
+    @Column (name="profile") val profile: String,
+    @ManyToMany @JoinTable (
         name = "signups",
         joinColumns = [JoinColumn(name = "snowboarder_id")],
         inverseJoinColumns = [JoinColumn(name = "event_id")]
-    )
-    val events: List<Event> = mutableListOf()
+    ) val events: List<Event> = mutableListOf(),
 )
