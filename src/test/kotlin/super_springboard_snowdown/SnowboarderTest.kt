22e3b6a52456a273f9858a9404a5b5cb75e7f74d
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-21 20:44:11 +0000

    created setup function - tests can be in any order

--- src/test/kotlin/super_springboard_snowdown/SnowboarderTest.kt
+++ src/test/kotlin/super_springboard_snowdown/SnowboarderTest.kt
@@ -1,29 +1,33 @@
 package super_springboard_snowdown
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import org.springframework.boot.test.context.SpringBootTest
 import super_springboard_snowdown.models.Event
 import super_springboard_snowdown.models.Snowboarder
-import super_springboard_snowdown.repositories.EventRepository
 import super_springboard_snowdown.repositories.SnowboarderService
 
 @SpringBootTest
 class SnowboarderTest(
-    private val snowboarderService: SnowboarderService,
-    private val eventRepository: EventRepository
+    private val snowboarderService: SnowboarderService
 ): StringSpec() {
-    private val craigMcMo = Snowboarder(
-        name = "Craig McMorris",
-        age = 29, profile = "https://upload.wikimedia.org/wikipedia/commons/6/65/Craig_McMorris_%282011%29.jpg"
-    )
 
-    private val railsJam = Event(
-        name = "Rails Jam",
-        location = "Buttermilk Mountain",
-        prizeMoney = 9000
-    )
+    lateinit var craigMcMo:Snowboarder
+    lateinit var railsJam: Event
+
+    override fun beforeEach(testCase: TestCase) {
+        craigMcMo = Snowboarder(
+            name = "Craig McMorris",
+            age = 29, profile = "https://upload.wikimedia.org/wikipedia/commons/6/65/Craig_McMorris_%282011%29.jpg"
+        )
+        railsJam = Event(
+            name = "Rails Jam",
+            location = "Buttermilk Mountain",
+            prizeMoney = 9000
+        )
+    }
 
     init {
         "Snowboarder should have name, age and img url" {
@@ -34,20 +38,18 @@ class SnowboarderTest(
             craigMcMo.id shouldBe null
         }
 
-        "Snowboarder should start with no events" {
-            craigMcMo.events.size shouldBe 0
-        }
-
         "Can add events to snowboarder" {
             craigMcMo.events.add(railsJam)
             craigMcMo.events.size shouldBe 1
         }
 
+        "Snowboarder should start with no events" {
+            craigMcMo.events.size shouldBe 0
+        }
+
         "Can save Craig to DB, should have id" {
-            eventRepository.save(railsJam)
             snowboarderService.post(craigMcMo)
             craigMcMo.id shouldNotBe null
         }
-
     }
 }
