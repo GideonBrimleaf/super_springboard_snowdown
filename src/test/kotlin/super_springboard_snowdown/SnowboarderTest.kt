795e72838e0b40a00f47afc9aa4447ad075b9703
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-21 17:12:55 +0000

    removes redundant annotation

--- src/test/kotlin/super_springboard_snowdown/SnowboarderTest.kt
+++ src/test/kotlin/super_springboard_snowdown/SnowboarderTest.kt
@@ -2,9 +2,7 @@ package super_springboard_snowdown
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class SnowboarderTest: StringSpec({
     "length should return size of string" {
         "hello".length shouldBe 5
