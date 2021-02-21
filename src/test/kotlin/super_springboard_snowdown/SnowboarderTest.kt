90cdedde5ac6032caa17e4e7f9a803058ca85754
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-21 17:33:46 +0000

    setup added

--- src/test/kotlin/super_springboard_snowdown/SnowboarderTest.kt
+++ src/test/kotlin/super_springboard_snowdown/SnowboarderTest.kt
@@ -4,15 +4,17 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import super_springboard_snowdown.models.Snowboarder
 
-class SnowboarderTest: StringSpec({
-    "Snowboarder should have name, age and img url" {
-        val craigMcMo = Snowboarder(
-            name="Craig McMorris",
-            age=29, profile="https://upload.wikimedia.org/wikipedia/commons/6/65/Craig_McMorris_%282011%29.jpg"
-        )
+class SnowboarderTest: StringSpec() {
+    private val craigMcMo = Snowboarder(
+        name = "Craig McMorris",
+        age = 29, profile = "https://upload.wikimedia.org/wikipedia/commons/6/65/Craig_McMorris_%282011%29.jpg"
+    )
 
-        craigMcMo.name shouldBe "Craig McMorris"
-        craigMcMo.age shouldBe 29
-        craigMcMo.profile shouldBe "https://upload.wikimedia.org/wikipedia/commons/6/65/Craig_McMorris_%282011%29.jpg"
+    init {
+        "Snowboarder should have name, age and img url" {
+            craigMcMo.name shouldBe "Craig McMorris"
+            craigMcMo.age shouldBe 29
+            craigMcMo.profile shouldBe "https://upload.wikimedia.org/wikipedia/commons/6/65/Craig_McMorris_%282011%29.jpg"
+        }
     }
-})
+}
