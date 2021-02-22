f2df1506592e98334a144a1f6790eca0c1ba60a6
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-22 21:08:56 +0000

    refactors template layouts to allow non-login template

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.pebbletemplates:pebble-spring-boot-starter:3.1.4")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.security:spring-security-test")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5:4.4.1")
