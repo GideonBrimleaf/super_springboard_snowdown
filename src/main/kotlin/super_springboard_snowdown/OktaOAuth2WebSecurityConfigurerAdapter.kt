71082eb22fd9c57892cd4bc940b9f4dd52820932
GideonBrimleaf <chrisdryden1@gmail.com>
2021-02-24 17:46:46 +0000

    enables successful logouts

--- src/main/kotlin/super_springboard_snowdown/OktaOAuth2WebSecurityConfigurerAdapter.kt
+++ src/main/kotlin/super_springboard_snowdown/OktaOAuth2WebSecurityConfigurerAdapter.kt
@@ -1,14 +1,16 @@
 package super_springboard_snowdown
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
 
 @Configuration
 class OktaOAuth2WebSecurityConfigurerAdapter: WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
-        http?.authorizeRequests()
+        http?.csrf()?.disable()?.authorizeRequests()
             ?.antMatchers("/")?.permitAll()
             ?.anyRequest()?.authenticated()
             ?.and()?.oauth2Login()
